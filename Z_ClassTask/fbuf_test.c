b5179a206b9f5b1dc931091388fab8080e84e5f5
Yang Shuaige <316952315@qq.com>
2024-04-08 09:56:00 +0800

    new file:   Z_ClassTask/fbuf_test.c 	new file:   Z_ClassTask/fbuf_test.exe

---
+++ Z_ClassTask/fbuf_test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("Nothing is impossible.\n");
+    ;
+    printf("Impossible is nothing.");
+    for (;;)
+        ;
+    return 0;
+}
